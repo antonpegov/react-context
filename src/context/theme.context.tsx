3da64eaad3e6182b7a5df259cb30b5aeb5f18a51
Anton Pegov <antonpegov@gmail.com>
2020-07-30 04:11:53 +0200

    Connect components to Context Data

--- src/context/theme.context.tsx
+++ src/context/theme.context.tsx
@@ -1,18 +1,20 @@
 import React, { Component, createContext } from 'react';
 
-export const ThemeContext = createContext({});
+const state = {
+  isLightTheme: true,
+  light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
+  dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
+}
+
+export const ThemeContext = createContext({...state, toggleTheme: () => {}});
 
 class ThemeContextProvider extends Component {
-  state = {
-    isLightTheme: true,
-    light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
-    dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
-  }
+  state = state;
 
   toggleTheme= () => {
     this.setState({ isLightTheme: !this.state.isLightTheme });
   }
-  
+
   render() { 
     return (
       <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
