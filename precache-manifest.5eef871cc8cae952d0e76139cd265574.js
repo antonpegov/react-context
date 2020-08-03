675d87d2da3d021420dac0c4b5b84d5a1ecc8263
Anton Pegov <antonpegov@gmail.com>
2020-08-03 11:43:57 +0200

    Updates

---
+++ precache-manifest.5eef871cc8cae952d0e76139cd265574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8fb07c65c9e2722ecf4dab80e0713d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b6aacb49d9bc60f319f",
+    "url": "/static/css/main.26c9701f.chunk.css"
+  },
+  {
+    "revision": "4cd14bfae4f9685a1c0d",
+    "url": "/static/js/2.bb71be7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb71be7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6aacb49d9bc60f319f",
+    "url": "/static/js/main.0ca7a707.chunk.js"
+  },
+  {
+    "revision": "5a36f61a7d32726947aa",
+    "url": "/static/js/runtime-main.a8655df2.js"
+  }
+]);
